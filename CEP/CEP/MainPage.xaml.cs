7c2dd06fed65c10cc247559015364bbca84fd585
Fabricio Curvello <tiofab@gmail.com>
2018-06-28 13:41:49 -0300

    App CEP versao 1.0

---
+++ CEP/CEP/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using CEP.Servico;
+using CEP.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            //BOTAO.Clicked += BuscarCEP;
+		}
+
+        private void BuscarCEP(object sender, EventArgs args) 
+        {
+            string cep = "";
+            
+            //cep recebe o texto da caixa de texto (Entry), retirando espaços iniciais e finais (trim)
+            if (CEP.Text != null) {
+                cep = CEP.Text.Trim();
+            }
+
+            //Validações
+            if (isValidCEP(cep)) {
+                try {
+
+                    //Passado o cep para o serviço ViaCEP, será retornado o obj Endereço.
+                    Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+
+                    if (end != null) {
+
+                        //Inserindo texto na label
+                        RESULTADO.Text = string.Format("Endereço: {0}, {1} - {2}, {3}",
+                            end.logradouro, end.bairro, end.localidade, end.uf);
+                    } else {
+                        DisplayAlert("CEP inexistente", "Não foi econtrado endereço correspondente ao CEP informado: " + cep, "OK");
+                        RESULTADO.Text = "CEP " + cep + " não localizado.";
+                    }
+
+                } catch (Exception e) {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+            }
+            
+        }//fim BuscarCEP()
+
+        //Validações
+        private bool isValidCEP(string cep) 
+        {
+            bool valido = true;
+
+            //CEP tem que ter algo digitado pelo usuário
+            if (cep.Equals("")) {
+                //ERRO
+                valido = false;
+                DisplayAlert("ERRO", "Por favor digite o CEP.", "OK");
+                RESULTADO.Text = "";
+                return valido;
+            }
+
+            //CEP tem que ter 8 dígitos
+            if (cep.Length != 8) {
+                //ERRO
+                valido = false;
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve conter 8 caracteres.", "OK");
+                RESULTADO.Text = "CEP " + cep + " inválido.";
+                return valido;
+            }
+
+            //CEP tem que ser somente números
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP)) {
+                //ERRO
+                valido = false;
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas por números.", "OK");
+                RESULTADO.Text = "CEP " + cep + " inválido.";
+                return valido;
+            }
+
+            return valido;
+        } //fim isValidCEP()
+
+	}
+}
---
+++ CEP/CEP/Servico/ViaCepServico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net; //WebClient - Para buscar o endereço na web pelo CEP
+using CEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace CEP.Servico
+{
+
+    /*
+     * Esta classe tem o propósito de fazer o download da 
+     * informação e converter para o formato útil ao projeto
+     */
+    public class ViaCepServico
+    {
+
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep) 
+        {
+            string EnderecoURLComCep = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient(); //using System.Net;
+
+            //JSON retornado pelo WebClient é convertido para string
+            string Conteudo = wc.DownloadString(EnderecoURLComCep);
+
+            //Converter string em objeto do tipo Endereco 
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            //Tratamento de errro
+            if (end.cep == null) return null;
+
+            return end;
+        }
+
+    }
+}
